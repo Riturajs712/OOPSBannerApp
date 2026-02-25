999fcddab78273bc70fd614d73b0095b6afe2b4e
riturajs712 <kingrituraj712@gmail.com>
2026-02-26 00:01:57 +0530

    Modified Program

--- OOPSBannerUC4.java
+++ OOPSBannerUC4.java
@@ -4,14 +4,13 @@ public class OOPSBannerUC4 {
 
         String[] lines = new String[7];
 
-
-        lines[0] = String.join(" ", "  ***  ", " ***** ");
-        lines[1] = String.join(" ", " *   * ", " *     ");
-        lines[2] = String.join(" ", "*     *", "*      ");
-        lines[3] = String.join(" ", "*     *", " ***** ");
-        lines[4] = String.join(" ", "*     *", "      *");
-        lines[5] = String.join(" ", " *   * ", " *    *");
-        lines[6] = String.join(" ", "  ***  ", " ***** ");
+        lines[0] = String.join("  ", "  ***  ", "  ***  ", " ***** ", " ***** ");
+        lines[1] = String.join("  ", " *   * ", " *   * ", " *   * ", " *     ");
+        lines[2] = String.join("  ", "*     *", "*     *", "*     *", "*     ");
+        lines[3] = String.join("  ", "*     *", "*     *", " ***** ", " ***** ");
+        lines[4] = String.join("  ", "*     *", "*     *", "*      ", "      *");
+        lines[5] = String.join("  ", " *   * ", " *   * ", " *      ", " *    *");
+        lines[6] = String.join("  ", "  ***  ", "  ***  ", " *      ", " ***** ");
 
         for (String line : lines) {
             System.out.println(line);
